4f2c8d6cd071a5c0734857f384fbf3ea87fa3c85
Mefamex <131618733+Mefamex@users.noreply.github.com>
2025-01-29 08:38:31 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Created on Friday, July 19 15:30:00 2024
+# @author: mefamex
+
+# project_name = "image-meta-dataset"
+# project_version = "1.0.0"
+# project_author = "Mefamex"
+# project_date = "29.01.2025"
+# project_description = "This is a project description"
+# project_license = "MIT"
+# project_repository = "https://github.com/Mefamex/image-meta-dataset"
+# project_url = "https://mefamex.com/projects/image-meta-dataset"
+
+# Importing libraries
+import os
+
