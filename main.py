ec712d3a5ef20b9d9e313a76994b3f22ca1bd170
Mefamex <131618733+Mefamex@users.noreply.github.com>
2025-01-30 07:49:00 +0300

    u

--- main.py
+++ main.py
@@ -48,52 +48,21 @@ def display_metadata(image_path):
                 print(f"{tag}: {value}")
     print("............................................")
 
-def modify_image_metadata(image_path, output_path):
+def delete_image_metadata(image_path, output_path):
     if not os.path.exists(image_path):
         print(f"Hata: {image_path} bulunamadı.")
         return
-    
     image = Image.open(image_path)
     exif_bytes = image.info.get("exif")
     exif_data = piexif.load(exif_bytes) if exif_bytes else {"0th": {}, "Exif": {}, "GPS": {}, "Interop": {}, "1st": {}, "thumbnail": None}
-    
-    print("Mevcut Metadata'yı Değiştirin:")
-    for ifd in exif_data:
-        if exif_data[ifd] is None or isinstance(exif_data[ifd], bytes):
-            continue
-        for tag in list(exif_data[ifd].keys()):
-            try:
-                tag_name = piexif.TAGS[ifd][tag]['name']
-                current_value = exif_data[ifd][tag]
-                if isinstance(current_value, bytes):
-                    current_value = current_value.decode(errors='ignore')
-                new_value = input(f"{tag_name} için yeni değer ({current_value}): ")
-                if new_value:
-                    exif_data[ifd][tag] = new_value.encode() if isinstance(exif_data[ifd][tag], bytes) else new_value
-            except KeyError:
-                continue
-    
-    print("Yeni Metadata Ekleyin:")
-    metadata_keys = {k: v['name'] for d in piexif.TAGS.values() for k, v in d.items()}
-    while True:
-        print("Eklenebilecek metadata türleri:")
-        for key, name in metadata_keys.items():
-            print(f"{key}: {name}")
-        
-        key_input = input("Eklemek istediğiniz metadata anahtarını girin (veya çıkmak için 'q' yazın): ")
-        if key_input.lower() == 'q':
-            break
-        
-        if key_input.isdigit() and int(key_input) in metadata_keys:
-            value_input = input(f"{metadata_keys[int(key_input)]} için değer girin: ")
-            exif_data["0th"][int(key_input)] = value_input.encode() if isinstance(value_input, str) else value_input
-    
     exif_bytes = piexif.dump(exif_data)
     image.save(output_path, exif=exif_bytes)
-    print(f"Yeni metadata ile {output_path} olarak kaydedildi.")
+    print(f"{image_path} dosyasındaki metadata başarıyla silindi ve {output_path} dosyasına kaydedildi.")
+    print("............................................")
 
 # Örnek Kullanım
-image_path = "a.webp"
-output_path = "aa.webp"
+image_path = "a.jpg"
+output_path = "aa.jpg"
 display_metadata(image_path)
-#modify_image_metadata(image_path, output_path)
+delete_image_metadata(image_path, output_path)
+display_metadata(output_path)
